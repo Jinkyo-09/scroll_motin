cdb90ccab42fa74b9a6ad4882064edca1eef7c94
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-27 16:12:38 +0900

    버튼 클릭 시 해당 순번의 섹션 위치로 자동 이동

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const secs = document.querySelectorAll('section');
+const btns = document.querySelectorAll('ul li');
+const speed = 500;
+let posArr = [];
+
+//로딩이 되지마자 스크롤 이동해야 되는 section의 세로 위치값을 배열에 저장
+secs.forEach((sec) => posArr.push(sec.offsetTop));
+
+//버튼 반복돌면서 이벤트 연결
+btns.forEach((btn, idx) => {
+	//각 버튼 클릭시 클릭한 순번의 세로 섹션 배열 위치값으로 스크롤 모션 이동
+	btn.addEventListener('click', () => {
+		new Anime(window, { scroll: posArr[idx] }, { duration: speed });
+	});
+});
+
 function splitText(selector, interval = 0, delay = 0) {
 	let count = 0;
 	const txt = selector.innerText;
